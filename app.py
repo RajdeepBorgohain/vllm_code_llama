84210239feb38f32cedb0d506333dfad641e094a
rajdeep <rajdeep@rajdeeps-MacBook-Air.local>
2023-11-03 01:07:15 +0530

    template for code llama

---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+from vllm import SamplingParams
+from vllm import LLM
+from huggingface_hub import snapshot_download
+
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.template = """SYSTEM: You are a helpful assistant.
+        USER: {}
+        ASSISTANT: """
+        snapshot_download(
+            "codellama/CodeLlama-34b-Python-hf",
+            local_dir="/model",
+            token="<<your_token>>",
+        )
+        self.llm = LLM("/model")
+    
+    def infer(self, inputs):
+        print("inputs[questions] -->", inputs["questions"], flush=True)
+        prompts = [self.template.format(inputs["questions"])]
+        print("Prompts -->", prompts, flush=True)
+        sampling_params = SamplingParams(
+            temperature=0.1,
+            top_p=1,
+            max_tokens=800,
+            presence_penalty=1.15,
+        )
+        result = self.llm.generate(prompts, sampling_params)
+        result_output = [output.outputs[0].text for output in result]
+
+        return {"result": result_output[0]}
+
+    def finalize(self, args):
+        pass
